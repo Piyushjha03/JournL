fcc03673558eaac1e47d2a3352c0c8ca67963ecf
Piyushjha03 <jhapiyush2305@gmail.com>
2024-02-18 15:58:19 +0530

    Remove secure flag from createToken function

--- server/controller/users.controller.js
+++ server/controller/users.controller.js
@@ -74,7 +74,6 @@ async function httpPostUser(req,res){
 function createToken(id){
     return jwt.sign({id}, process.env.JWT_SECRET,{
         expiresIn: 3* 24 * 60 * 60, //3 days in seconds
-        secure:true,
     }); 
 }
 
