375c01444aae0587353812e1183720f0794fc1b1
Piyushjha03 <jhapiyush2305@gmail.com>
2024-02-19 06:07:36 +0530

    Update socket configuration

--- client/src/components/eachChat/socket.js
+++ client/src/components/eachChat/socket.js
@@ -1,6 +1,4 @@
 import { io } from 'socket.io-client';
 
 const Endpoint='https://journl-server.vercel.app'
-export const socket=io(Endpoint,{
-    transports: [ "websocket" ]
-})
+export const socket=io(Endpoint)
