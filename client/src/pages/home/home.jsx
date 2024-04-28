d6137f6c8629ec311225db34e7914e873bc08586
Piyushjha03 <jhapiyush2305@gmail.com>
2024-04-28 17:50:13 +0530

    Update home.jsx and home.module.css

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,49 +1,69 @@
-import { Outlet, useNavigate } from "react-router-dom"
-import { Navbar } from "../../components/navbar/navbar"
-import style from './home.module.css'
-import { useSelector } from "react-redux"
-import { FaUserEdit } from "react-icons/fa"
-
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import { Navbar } from "../../components/navbar/navbar";
+import style from "./home.module.css";
+import { useSelector } from "react-redux";
+import { FaSignOutAlt, FaUserEdit } from "react-icons/fa";
 
 function Home() {
-  const nav=useNavigate()
-  const currentUser= useSelector(state=>state?.userDetailsSlice?.userInfo)
+  const nav = useNavigate();
+  const currentUser = useSelector((state) => state?.userDetailsSlice?.userInfo);
   return (
     <>
+      <div className={style.home_page_wrapper}>
+        <div className={style.navbar}>
+          <Navbar />
+        </div>
+        <div className={style.main_content}>
+          <Outlet />
+        </div>
+        <div className={style.right_content}>
+          <>
+            <div className={style.myAccount_wrapper}>
+              <div className={style.userInfo}>
+                <button
+                  onClick={() => {
+                    nav("/login");
+                  }}
+                >
+                  <FaSignOutAlt />
+                </button>
 
-    <div className={style.home_page_wrapper}>
-    <div className={style.navbar}>
-    <Navbar/>
-    </div>
-    <div className={style.main_content}>
-    <Outlet/>
-    </div>
-    <div className={style.right_content}>
-    <>
-    <div className={style.myAccount_wrapper}>
-
-      <div className={style.userInfo}>
-      <button onClick={()=>{nav('/myAccount')}}><FaUserEdit/></button>
-        <div className={style.profilePicture_container}>
-            <img src={`${currentUser?.profilePicture?.includes('cloudinary') ? currentUser.profilePicture : `${currentUser.profilePicture}/60` }`} alt="pfp"  draggable="false"/>
+                <div
+                  className={style.profilePicture_container}
+                  onClick={() => {
+                    nav("/myAccount");
+                  }}
+                >
+                  <img
+                    src={`${
+                      currentUser?.profilePicture?.includes("cloudinary")
+                        ? currentUser.profilePicture
+                        : `${currentUser.profilePicture}/60`
+                    }`}
+                    alt="pfp"
+                    draggable="false"
+                  />
+                </div>
+                <div
+                  className={style.userdetails}
+                  onClick={() => {
+                    nav("/myAccount");
+                  }}
+                >
+                  <div
+                    className={style.userName}
+                  >{`${currentUser.userName}`}</div>
+                  <div className={style.Name}>
+                    {currentUser.firstName + " " + currentUser.lastName}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
         </div>
-        <div className={style.userdetails}>
-        <div className={style.userName} >{`${currentUser.userName}`}</div>
-        <div className={style.Name} >{currentUser.firstName + ' ' + currentUser.lastName }</div>
-        </div>
-        
-        </div>
-    </div>
+      </div>
     </>
-    
-    </div>
-    </div>
-
-    
-    
-    </>
-   
-  )
+  );
 }
 
-export default Home
+export default Home;
